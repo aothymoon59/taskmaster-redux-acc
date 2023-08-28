79a7abb61732108caf3faed9c08669a7bf3dcaaf
aothymoon59 <mmoon8745@gmail.com>
2023-08-28 17:45:45 +0600

    Add task modal and generic modal connection

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,8 +1,10 @@
-const AddTaskModal = () => {
-    return (
-        <div>
+import Modal from "../ui/Modal";
 
-        </div>
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+    return (
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title='Programming Hero'>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi error magni quos iste labore, id fugit mollitia sit! Officiis quod ab culpa perferendis. Excepturi in odio rerum? Nesciunt, exercitationem tenetur?</p>
+        </Modal>
     );
 };
 
